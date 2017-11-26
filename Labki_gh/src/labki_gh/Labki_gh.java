a711c36cbae99d9ce9a39ddd712416a5274f3884
KD <testowy@test.com>
2017-11-26 15:04:02 +0100

    Added alpha implementation with ubfixed numerofwords

--- Labki_gh/src/labki_gh/Labki_gh.java
+++ Labki_gh/src/labki_gh/Labki_gh.java
@@ -1,21 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package labki_gh;
 
-/**
- *
- * @author student
- */
-public class Labki_gh {
+import java.io.*;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+class Labki_gh {
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            System.out.println("Podaj ścieżkę dostępu do pliku: ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String sciezka = br.readLine();
+            FileReader in = new FileReader(sciezka);
+            System.out.println("Ilość linii: " + numberOfLines(in));
+            System.out.println("Liczba slów: " + numberOFWords(in));
+            in.close();
+            br.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Plik nie istnieje");
+            System.exit(1);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
-    
+
+    public static int numberOfLines(FileReader r) throws IOException {
+        LineNumberReader ln = new LineNumberReader(r);
+        int count = 0;
+        while (ln.readLine() != null) {
+            count++;
+        }
+        return count;
+    }
+
+    public static int numberOFWords(FileReader r) {
+        Scanner skaner = new Scanner(r);
+        int count = 0;
+        while (skaner.hasNext()) {
+            skaner.next();
+            count++;
+        }
+        skaner.close();
+        return count;
+    }
 }
