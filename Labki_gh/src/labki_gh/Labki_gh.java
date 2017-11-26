f2126450b3bb5af97e60f856a1604c264bbfe8e2
ee <ss>
2017-11-26 14:52:58 +0100

    pierwszy commit

--- Labki_gh/src/labki_gh/Labki_gh.java
+++ Labki_gh/src/labki_gh/Labki_gh.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package labki_gh;
 
-/**
- *
- * @author student
- */
-public class Labki_gh {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Labki_gh 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        
     }
     
 }
